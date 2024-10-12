f0d6f89519407a6a9b718ef85530e63fd8c530ed
Umar Haqqui <umar.haqqui@idenfo.com>
2024-10-13 03:38:31 +0500

    final changes

--- utils/database.js
+++ utils/database.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 export const connectDB = () => {
     mongoose.connect(process.env.MONGO_URL, {
         dbName: "backend"
-    }).then(() => {
-        console.log("Connected to database");
+    }).then((c) => {
+        console.log(`Connected to database: ${c.connection.host}`);
     }).catch((err) => {
         console.log("Error connecting to database: ", err);
     });
